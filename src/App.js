0bd59baffb8bd3c018cb93b41ce92d45105e2e13
rike14 <henriquemk00@gmail.com>
2022-09-18 23:37:30 -0300

    add Drawer and handleClick

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ import Customers from './pages/Customers'
 
 function App() {
   return (
-    <TemplateDefault>
-      <Router>
+    <Router>
+      <TemplateDefault>
         <Switch>
           <Route path="/customers">
             <TemplatePage title="Customers" Component={Customers} />
@@ -25,8 +25,8 @@ function App() {
             <TemplatePage title="Home" Component={Home} />
           </Route>
         </Switch>
-      </Router>
-    </TemplateDefault>
+      </TemplateDefault>
+    </Router>
   )
 }
 
--- src/partials/Header/Header.js
+++ src/partials/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     AppBar,
     Box,
@@ -6,36 +6,74 @@ import {
     Typography,
     Button,
     IconButton,
+    Drawer,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
 } from '@mui/material'
 
+import { useHistory } from "react-router-dom";
+
 import MenuIcon from '@mui/icons-material/Menu'
+import HomeIcon from '@mui/icons-material/Home'
+import GroupAddIcon from '@mui/icons-material/GroupAdd'
 import './Header.css'
 
 
 
 const Header = () => {
+    const history = useHistory()
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const handleToggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
+
+    const handleMenuClick = route => {
+        history.push(route)
+        handleToggleMenu()
+    }
 
     return(
-        <Box sx={{ flexGrow: 1 }} >
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} >
-                        Home
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-        </Box>
-
+        <>
+            <Box sx={{ flexGrow: 1 }} >
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                            onClick={() => handleToggleMenu()}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} >
+                            Home
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <Drawer open={menuOpen} onClose={() => handleToggleMenu()}>
+                <List>
+                    <ListItem button onClick={() => handleMenuClick('/')}>
+                        <ListItemIcon>
+                            <HomeIcon/>
+                        </ListItemIcon>
+                        <ListItemText>Home</ListItemText>
+                    </ListItem>
+                    <ListItem button onClick={() => handleMenuClick('/customers')} >
+                        <ListItemIcon>
+                            <GroupAddIcon/>
+                        </ListItemIcon>
+                        <ListItemText>Customers</ListItemText>
+                    </ListItem>
+                </List>
+            </Drawer>
+        </>
     )
 }
 
